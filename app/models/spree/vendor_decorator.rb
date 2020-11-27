45b7595f2df0f8e3efe6ebb64132052f57f55c6b
Omair Azam <omairazam@punch.cool>
2020-11-27 21:54:50 +0500

    fix: Remove vendor relationship from slider

--- app/models/spree/vendor_decorator.rb
+++
@@ -1,9 +0,0 @@
-# module Spree
-#   module VendorDecorator
-#     def self.prepended(base)
-#       base.has_many :slides
-#     end
-#   end
-# end
-# ::Spree::Vendor.prepend(Spree::VendorDecorator)
-
